464cd4301d0080f7807b85d2aacca7787122f22a
mercurynxt18 <brokenheart98998@gmail.com>
2026-07-11 23:22:58 +0700

    Update AutoEquip.lua

--- AutoEquip.lua
+++ AutoEquip.lua
@@ -1,4 +1,3 @@
-wait(10)
 local Players = game:GetService("Players")
 local localPlayer = Players.LocalPlayer
 local WEAPON_NAME = "Bat" -- Double-check the weapon name to make sure it is actually "Bat"
